f4237ed928bdeeea625bb22fff3b804a004b180b
jojoner16 <laybats.jonathan@orange.fr>
2022-07-15 19:52:35 +0200

    start project

---
+++ starterOnly/js/validationForm.js
@@ -0,0 +1,11 @@
+// DOM Elements validations formulaire
+const firstName = document.getElementById('first');
+const lastName = document.getElementById('last');
+const email = document.getElementById('email');
+const quantity = document.getElementById('quantity');
+const birthdate = document.getElementById('birthdate');
+const allLocations = document.getElementById('locations');
+const locations = document.querySelectorAll('#locations .checkbox-input');
+const checkbox1 = document.getElementById('checkbox1');
+const input = document.getElementsByClassName('text-control');
+const form = document.getElementById('form');
--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,3 +1,25 @@
+// DOM Elements
+const modalBg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+const closeBtn = document.getElementsByClassName("close");
+
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// launch modal form
+function launchModal() {
+  modalBg.style.display = "block";
+}
+
+// close modal form
+function closeModal() {
+  modalBg.style.display = "none";
+}
+closeBtn[0].addEventListener("click", closeModal);
+
+// ------ display nav responsive ------ //
+// edit nav
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -6,18 +28,3 @@ function editNav() {
     x.className = "topnav";
   }
 }
-
-// DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
-
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
-// launch modal form
-function launchModal() {
-  modalbg.style.display = "block";
-}
-
-
