e51be49e0e775fc2a5978f851946a268a549df11
jojoner16 <laybats.jonathan@orange.fr>
2022-07-21 09:29:34 +0200

    fermeture de la popup d'inscription

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -4,22 +4,22 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeBtn = document.getElementsByClassName("close");
 
-// launch modal event
+  // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-// launch modal form
+  // launch modal form
 function launchModal() {
   modalBg.style.display = "block";
 }
 
-// close modal form
+  // close modal form
 function closeModal() {
   modalBg.style.display = "none";
 }
 closeBtn[0].addEventListener("click", closeModal);
 
-// ------ display nav responsive ------ //
-// edit nav
+  // ------ display nav responsive ------ //
+  // edit nav
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
