c9b02f75cc3f043bcda0caf0f05c77e3cc3b4c9b
jojoner16 <laybats.jonathan@orange.fr>
2022-07-22 14:30:28 +0200

    fermeture popup confirmation d'inscription par la croix et le bouton

--- starterOnly/js/confirmationSubmit.js
+++ starterOnly/js/confirmationSubmit.js
@@ -1,2 +1,23 @@
     // DOM elements confirmation submit
+const modalSubmit = document.getElementsByClassName('container-confirmation-submit');
+const closeModalSubmit = document.getElementsByClassName('close-modal-submit');
+const closeBtnConfirmation = document.getElementById('close-btn-confirmation');
+
+
+            // confirmation du submit
+
+    // afficher modal
+function displayModalSubmit() {
+    modalbg.style.display = 'none';
+    modalSubmit[0].style.display = 'block';
+}
+
+    // fermer modal
+function closeSubmit() {
+    modalSubmit[0].style.display = 'none';
+}
+
+    // évènement fermeture modal
+closeModalSubmit[0].addEventListener('click', closeSubmit);
+closeBtnConfirmation.addEventListener('click', closeSubmit);
 
